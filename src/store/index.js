d692e105e208377e8319ac1d80da578f3d5c8afd
Mikael <nilssonmikael90@gmail.com>
2020-03-14 14:54:10 +0100

    Created a base structure of the Vuex store and can now store the name of the game and game master

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,83 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    game: {
+      id: null,
+      name: null,
+      game_master_name: null,
+      started: false,
+      finished: false,
+      ip: null,
+      created: null
+    },
+    beers: [],
+    players: []
   },
   mutations: {
+    setGame(state, payload) {
+      state.game = payload;
+    },
+    setBeers(state, payload) {
+      state.beers = payload;
+    },
+    addBeer(state, payload) {
+      state.beers.push(payload);
+    },
+    removeBeer(state, index) {
+      state.beers.splice(index, 1);
+    },
+    setUsers(state, payload) {
+      state.users = payload;
+    },
+    addUser(state, payload) {
+      state.users.push(payload);
+    },
+    removeUser(state, index) {
+      state.users.splice(index, 1);
+    }
   },
   actions: {
+    storeGame({ commit }, payload) {
+      const game = { ...payload, created: Date() };
+      commit("setGame", game);
+
+      return true;
+    },
+    updateGame({ commit }, payload) {
+      commit("setGame", { ...payload });
+    },
+    setBeers({ commit }, payload) {
+      commit("setBeers", { ...payload });
+    },
+    addBeer({ commit }, payload) {
+      commit("addBeer", { ...payload });
+    },
+    removeBeer({ commit, getters }, beerId) {
+      const index = getters.getBeers.findIndex(beer => beer.id === beerId);
+      commit("removeBeer", index);
+    },
+    setUsers({ commit }, payload) {
+      commit("setUsers", { ...payload });
+    },
+    addUser({ commit }, payload) {
+      commit("addUser", { ...payload });
+    },
+    removeUser({ commit, getters }, userId) {
+      const index = getters.getUsers.findIndex(user => user.id === userId);
+      commit("removeUser", index);
+    }
   },
-  modules: {
-  }
-})
+  getters: {
+    getBeers(state) {
+      return state.beers;
+    },
+    getUsers(state) {
+      return state.users;
+    }
+  },
+  modules: {}
+});
--- src/views/CreateGame.vue
+++ src/views/CreateGame.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Create a new game</h1>
-    <div class="mb-4">
+    <div class="mb-6">
       <label for="gameTitle" class="block text-gray-700 font-bold mb-2">Enter the name of the game</label>
       <input
         type="text"
@@ -12,8 +12,24 @@
         required
       />
     </div>
+    <div class="mb-6">
+      <label for="gameMasterName" class="block text-gray-700 font-bold mb-2">Enter your name</label>
+      <input
+        type="text"
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="gameMasterName"
+        v-model="game_master_name"
+        placeholder="Your name"
+        required
+      />
+    </div>
     <div>
-      <router-link :to="{ name: 'GameType' }" class="btn btn-blue">Next</router-link>
+      <button
+        class="btn btn-blue btn:disabled"
+        :disabled="disabled"
+        @click.prevent="onStoreGame"
+      >Next</button>
+      <!-- <router-link :to="{ name: 'GameType' }" class="btn btn-blue">Next</router-link> -->
     </div>
   </div>
 </template>
@@ -22,8 +38,29 @@
 export default {
   data() {
     return {
-      title: ""
+      title: "",
+      game_master_name: ""
     };
+  },
+  computed: {
+    disabled() {
+      return this.title.trim() === "" || this.game_master_name.trim() === "";
+    }
+  },
+  methods: {
+    onStoreGame() {
+      const response = this.$store.dispatch("storeGame", {
+        title: this.title,
+        game_master_name: this.game_master_name
+      });
+
+      if (!response) {
+        alert("Something went wrong. Try again");
+        return;
+      }
+
+      this.$router.push({ name: "GameType" });
+    }
   }
 };
 </script>
