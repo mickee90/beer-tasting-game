4eb03a986055fe93da4dd261276d812157c98749
Mikael <nilssonmikael90@gmail.com>
2020-03-21 11:03:27 +0100

    Added 'finished game' guard on the JoinGame view

--- src/views/JoinGame.vue
+++ src/views/JoinGame.vue
@@ -1,31 +1,39 @@
 <template>
   <div v-if="game">
-    <h1>Waiting for the game to start</h1>
-    <div class="mb-8 mt-2">
-      <div
-        class="border-b border-gray-300 mb-4 pb-4"
-        v-for="(player, index) in players"
-        :key="player.id"
+    <div v-if="game.finished">
+      <h1>The game has ended</h1>
+      <router-link :to="{ name: 'Scoreboard' }" class="btn btn-blue"
+        >Go to scoreboard</router-link
       >
-        <div class="text-xl">
-          <div class="card-header">
-            Player #{{ index + 1 }} - {{ player.name }}
+    </div>
+    <div v-else>
+      <h1>Waiting for the game to start</h1>
+      <div class="mb-8 mt-2">
+        <div
+          class="border-b border-gray-300 mb-4 pb-4"
+          v-for="(player, index) in players"
+          :key="player.id"
+        >
+          <div class="text-xl">
+            <div class="card-header">
+              Player #{{ index + 1 }} - {{ player.name }}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="mb-8">
-      You can continue as soon as everyone has joined and the game master has
-      started the game
-    </div>
+      <div class="mb-8">
+        You can continue as soon as everyone has joined and the game master has
+        started the game
+      </div>
 
-    <button v-if="!game.started" class="btn btn-blue" disabled>
-      Waiting...
-    </button>
-    <button v-else class="btn btn-blue" @click="onStartGame">
-      Bring out the beer!
-    </button>
+      <button v-if="!game.started" class="btn btn-blue" disabled>
+        Waiting...
+      </button>
+      <button v-else class="btn btn-blue" @click="onStartGame">
+        Bring out the beer!
+      </button>
+    </div>
   </div>
 </template>
 
@@ -54,11 +62,9 @@ export default {
           game_id: store.getters.getGame.id
         },
         result(data) {
-          console.log(data);
           const game = data.data.game;
 
-          this.game = { ...this.game, started: game.started };
-          console.log(this.game);
+          this.game = { ...this.game, ...game };
           this.players = game.players;
         }
       }
