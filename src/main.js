e6b2373dbe8375859262fafcc4eee5364cda1a59
Mikael <nilssonmikael90@gmail.com>
2020-04-03 22:24:14 +0200

    Removed the option to scan beers.

--- src/main.js
+++ src/main.js
@@ -15,49 +15,12 @@ import VueBarcode from "@chenfengyuan/vue-barcode";
 import VueCarousel from "@chenfengyuan/vue-carousel";
 import Clipboard from "v-clipboard";
 
-import VueQuagga from "vue-quaggajs";
-
-// register component 'v-quagga'
-Vue.use(VueQuagga);
-// import VueApollo from "vue-apollo";
-
-// import ApolloClient from "apollo-client";
-// import { HttpLink } from "apollo-link-http";
-// import { InMemoryCache } from "apollo-cache-inmemory";
-
-// const getHeaders = () => {
-//   const headers = {};
-//   const token = window.localStorage.getItem("apollo-token");
-//   if (token) {
-//     headers.authorization = `Bearer ${token}`;
-//   }
-//   return headers;
-// };
-
-// const link = new HttpLink({
-//   uri: "https://beer-tasting-game.herokuapp.com/v1/graphql",
-//   fetch,
-//   headers: getHeaders()
-// });
-
-// const client = new ApolloClient({
-//   link: link,
-//   cache: new InMemoryCache({
-//     addTypename: true
-//   })
-// });
-
-// const apolloProvider = new VueApollo({
-//   defaultClient: client
-// });
-
 Vue.use(VueCarousel);
 Vue.component(VueBarcode.name, VueBarcode);
 
 Vue.config.productionTip = false;
 
 Vue.use(Clipboard);
-// Vue.use(VueApollo);
 
 new Vue({
   router,
--- src/views/ChooseBeersWithQuagga.vue
+++ src/views/ChooseBeersWithQuagga.vue
@@ -4,7 +4,7 @@
     <div>
       <div class="mb-4">
         <label for="beerSearch" class="block text-gray-700 font-bold mb-2"
-          >Search for the beer</label
+          >Search or scan for the beer</label
         >
 
         <BaseInputText
@@ -29,6 +29,24 @@
         </div>
       </div>
 
+      <div
+        class="w-1/2 text-center border-b border-gray-400 mx-auto mb-5 text-lg"
+      >
+        Or
+      </div>
+
+      <button
+        class="btn btn-gray"
+        @click="showBarcodeScanner = !showBarcodeScanner"
+      >
+        Scan barcode
+      </button>
+
+      <div id="interactive" class="viewport scanner">
+        <video />
+        <canvas class="drawingBuffer" />
+      </div>
+
       <div class="py-8" v-if="beers.length > 0">
         <chosen-beer-card
           v-for="beer in beers"
@@ -49,16 +67,80 @@
 import SearchBeerCard from "../components/Layout/SearchBeerCard";
 import ChosenBeerCard from "../components/Layout/ChosenBeerCard";
 import axios from "../axios/axiosUntappd";
+import Quagga from "quagga";
 
 export default {
   data() {
     return {
       searchWord: "",
       beerSearchResult: null,
-      beers: []
+      beers: [],
+      showBarcodeScanner: false,
+      quaggaState: {
+        inputStream: {
+          type: "LiveStream",
+          constraints: {
+            width: { min: 640 },
+            height: { min: 480 },
+            facingMode: "environment",
+            aspectRatio: { min: 1, max: 2 }
+          }
+        },
+        locator: {
+          patchSize: "medium",
+          halfSample: true
+        },
+        numOfWorkers: 2,
+        frequency: 10,
+        decoder: {
+          readers: ["ean_reader"]
+        },
+        locate: true
+      }
     };
   },
   methods: {
+    onDetected(result) {
+      console.log("detected: ", result);
+    },
+    onProcessed(result) {
+      let drawingCtx = Quagga.canvas.ctx.overlay;
+      let drawingCanvas = Quagga.canvas.dom.overlay;
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(
+            0,
+            0,
+            parseInt(drawingCanvas.getAttribute("width")),
+            parseInt(drawingCanvas.getAttribute("height"))
+          );
+          result.boxes
+            .filter(function(box) {
+              return box !== result.box;
+            })
+            .forEach(function(box) {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                color: "green",
+                lineWidth: 2
+              });
+            });
+        }
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+            color: "#00F",
+            lineWidth: 2
+          });
+        }
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(
+            result.line,
+            { x: "x", y: "y" },
+            drawingCtx,
+            { color: "red", lineWidth: 3 }
+          );
+        }
+      }
+    },
     onDelete(id) {
       const response = this.$store.dispatch("removeBeer", id);
       this.beerSearchResult = [];
@@ -136,6 +218,20 @@ export default {
   },
   created() {
     this.beers = this.$store.getters.getBeers;
+    console.log(Quagga);
+  },
+  mounted() {
+    Quagga.init(this.quaggaState, function(err) {
+      if (err) {
+        return console.error(err);
+      }
+      Quagga.start();
+    });
+    Quagga.onDetected(this.onDetected);
+    Quagga.onProcessed(this.onProcessed);
+  },
+  destroyed: function() {
+    Quagga.stop();
   },
   components: {
     ChosenBeerCard,
@@ -143,3 +239,15 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.viewport {
+  position: relative;
+}
+.viewport canvas,
+.viewport video {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+</style>
